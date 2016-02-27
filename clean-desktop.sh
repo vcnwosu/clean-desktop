c72b25967d85f1e706733821b088de59ca70d75e
vcnwosu <vcnwosu@gmail.com>
2016-02-27 01:09:16 -0500

    Update clean-desktop.sh
    Eliminated the stat command. After re-examining the code, it became obvious that checking for modification changes would be unnecessary, since the script also checks the ~/Desktop directory file list in order of most recent modification.

--- clean-desktop.sh
+++ clean-desktop.sh
@@ -10,9 +10,6 @@
 # manually added simply by editing the script.
 # Particularly, editing the MUS, PIC & VID arrays.
 
-# stat string
-S=$(stat -t ~/Desktop)
-
 # list of previous files in order of modification
 PFL=$(ls -c ~/Desktop)
 
@@ -24,58 +21,48 @@ VID=(mp4 mov flv avi)
 # initiate while loop
 while [ 0 -lt 1 ]; do
 
-	# new stat
-	NS=$(stat -t ~/Desktop)
+	# check to make sure file list is not the same
+	NFL=$(ls -c ~/Desktop);
+	if [ "$PFL" != "$NFL" ]; then
 
-	# check if ~/Desktop has been modified
-  	if [ "$S" != "$NS" ]; then
+		# sleep
+		sleep 3
 
-		# check to make sure file list is not the same
-		NFL=$(ls -c ~/Desktop);
-		if [ "$PFL" != "$NFL" ]; then
+		# directory has been modified
+		# grap file name
+		FILE=$(ls -c1 ~/Desktop | head -1)
 
-			# sleep
-			sleep 3
+		# grap file extension
+		EXT=$(ls -c1 ~/Desktop | head -1 | sed "s|.*\.||")
 
-			# directory has been modified
-			# grap file name
-			FILE=$(ls -c1 ~/Desktop | head -1)
+		# check file extension against music
+		for i in ${MUS[@]}; do
+			# extension found
+			if [ "$EXT" = "${i}" ]; then
+				# move the file to appropriate directory
+				$(mv ~/Desktop/${FILE} ~/Music)
+			fi
+		done
 
-			# grap file extension
-			EXT=$(ls -c1 ~/Desktop | head -1 | sed "s|.*\.||")
+		# check file extension against pictures
+		for i in ${PIC[@]}; do
+			# extension found
+			if [ "$EXT" = "${i}" ]; then
+				# move to appropriate directory
+				$(mv ~/Desktop/${FILE} ~/Pictures)
+			fi
+		done
 
-			# check file extension against music
- 			for i in ${MUS[@]}; do
-				# extension found
-				if [ "$EXT" = "${i}" ]; then
-					# move the file to appropriate directory
-					$(mv ~/Desktop/${FILE} ~/Music)
-				fi
-			done
-
-			# check file extension against pictures
-			for i in ${PIC[@]}; do
-				# extension found
-				if [ "$EXT" = "${i}" ]; then
-					# move to appropriate directory
-					$(mv ~/Desktop/${FILE} ~/Pictures)
-				fi
-			done
-
-			# check file extension against videos
-			for i in ${VID[@]}; do
-				# extension found
-				if [ "$EXT" = "${i}" ]; then
-					# move to appropriate directory
-					$(mv ~/Desktop/${FILE} ~/Videos)
-				fi
-			done
-
-		fi
+		# check file extension against videos
+		for i in ${VID[@]}; do
+			# extension found
+			if [ "$EXT" = "${i}" ]; then
+				# move to appropriate directory
+				$(mv ~/Desktop/${FILE} ~/Videos)
+			fi
+		done
 
 	fi
 
-	# set STAT to NEW_STAT
-	S=$NS
-
 done
+
